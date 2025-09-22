45d958a2ec5430c8c13e572bd1c2da1458cdce7f
Fateme Abdellahi <fateme.abdellahii@gmail.com>
2025-09-22 16:22:21 +0000

    add permission for products

---
+++ products/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user or request.user.is_staff
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
--- products/views.py
+++ products/views.py
@@ -3,9 +3,11 @@ from rest_framework import generics
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from .permissions import IsOwner
 
 class ProductListAPIView(generics.ListAPIView):
     serializer_class = ProductSerializer
+    permission_classes = [IsOwner]
 
     def get_queryset(self):
         return Product.objects.all()
