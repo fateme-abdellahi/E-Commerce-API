7b75c6907ee5790389e75bbb5e2d443f63baccc2
Fateme Abdellahi <fateme.abdellahii@gmail.com>
2025-09-22 16:17:23 +0000

    add list products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductListAPIView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all()
+    
