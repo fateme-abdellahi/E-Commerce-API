c60604159bd2f1024debbe5a1a259b445556ce75
Fateme Abdellahi <fateme.abdellahii@gmail.com>
2025-09-22 17:31:36 +0000

    add product list by id

--- products/views.py
+++ products/views.py
@@ -7,6 +7,18 @@ from .serializers import ProductSerializer
 from .permissions import IsOwner
 
 
+class ProductAPIView(APIView):
+    serializer_class = ProductSerializer
+    permission_classes = [IsOwner]
+
+    def get(self, request, pk, *args, **kwargs):
+        products = Product.objects.filter(id=pk)
+        if not products.exists():
+            return Response(status=404)
+        
+        serializer = self.serializer_class(products[0])
+
+        return Response(serializer.data)
     
     
 class ProductCreateAPIView(generics.CreateAPIView):
